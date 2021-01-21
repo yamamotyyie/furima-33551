953055c82b319838fb9be2e206434a7666f5cb59
yamamotyyie <remuxia1415erina@gmail.com>
2021-01-21 19:09:25 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :full_surname, presence: true
-  validates :full_name, presence: true
-  validates :half_surname, presence: true
-  validates :half_name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください'}do
+    validates :full_surname, presence: true
+    validates :full_name, presence: true
+  end
+
+  with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/, message: '全角(カタカナ)を使用してください'}do
+    validates :half_surname, presence: true
+    validates :half_name, presence: true
+  end
+
   validates :birthday, presence: true
 end
