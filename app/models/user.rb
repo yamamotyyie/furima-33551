343d28bde2f8c23ffe973d17d4affeb79a000a30
yamamotyyie <remuxia1415erina@gmail.com>
2021-01-21 15:46:27 +0900

    新規登録、エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :full_surname, presence: true
+  validates :full_name, presence: true
+  validates :half_surname, presence: true
+  validates :half_name, presence: true
+  validates :birthday, presence: true
 end
