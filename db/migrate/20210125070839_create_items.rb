e83ae295d002a2eb881a09b26875762850f43693
yamamotyyie <remuxia1415erina@gmail.com>
2021-01-25 16:20:44 +0900

    モデル、DBの作成

---
+++ db/migrate/20210125070839_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name, null: false
+      t.text :text, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :burden_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :ship_day_id, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
