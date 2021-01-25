1a95bda8b3613acbc9aa0520126c27270ba49313
yamamotyyie <remuxia1415erina@gmail.com>
2021-01-25 17:30:31 +0900

    DB修正

--- db/migrate/20210125070839_create_items.rb
+++ db/migrate/20210125070839_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.references :user, foreign_key: true, null: false
       t.string :item_name, null: false
       t.text :text, null: false
       t.integer :category_id, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_072851) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "item_name", null: false
     t.text "text", null: false
     t.integer "category_id", null: false
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_072851) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_01_25_072851) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
