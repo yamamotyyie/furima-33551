79d8128722e5782ab9af4d3fe33bd4bb4ec4ff0d
yamamotyyie <remuxia1415erina@gmail.com>
2021-01-26 19:20:09 +0900

    モデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :text
+    validates :price
+  end
+
+  validates :category_id,:status_id,:burden_id,:prefecture_id,:ship_day_id, numericality: { other_than: 1 } 
 
   belong_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
