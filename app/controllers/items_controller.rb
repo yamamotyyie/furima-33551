9f123fe773cf325dcac8a97c189f03994457790a
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-02 18:45:17 +0900

    出品者以外のユーザーの処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
