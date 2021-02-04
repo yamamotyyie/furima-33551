b7393b3be17f0a2a373e915dd0011a96bc1718b8
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-04 23:24:18 +0900

    住所の登録

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,24 @@
 class PurchasesController < ApplicationController
 def index
+  @purchase_address = PurchaseAddress.new
+  @item = Item.find(params[:item_id])
+end
+
+def create
+  @item = Item.find(params[:item_id])
+  @purchase_address = PurchaseAddress.new(purchase_params)
+  if @purchase_address.valid?
+    @purchase_address.save
+    redirect_to root_path
+ else
+   render action: :index
+ end
+end
+
+
+private
+
+def purchase_params
+  params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :number).merge(user_id: current_user.id,item_id: params[:item_id])
 end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code,:prefecture_id,:city,:address,:building,:number,:item_id,:user_id
+  attr_accessor :postal_code,:prefecture_id,:city,:address,:building,:number,:item_id,:user_id,:purchase_id
 
   with_options presence: true do
     validates :postal_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -9,10 +9,10 @@ class PurchaseAddress
     validates :number,format:{with: /\A\d{10}\z|\A\d{11}\z/}
   end
 
-  validates :prefecture_id,numericality: {other_than:0}
+  validates :prefecture_id,numericality: {other_than:1}
 
   def save
-    purchase = purchase.create(user_id:user_id,item_id:item_id)
+    purchase = Purchase.create(user_id:user_id,item_id:item_id)
     Address.create(postal_code: postal_code,  prefecture_id: prefecture_id, city: city, address: address, building: building, number: number, purchase_id: purchase.id)
   end
 end
