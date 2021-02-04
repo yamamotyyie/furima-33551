9818e3147e6f636e078cbad3b27b56ed1c3d4548
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-04 19:16:46 +0900

    データベース

--- db/migrate/20210203120209_create_addresses.rb
+++ db/migrate/20210203120209_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.references :purchase, foreign_key: true, null: false
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :number, null: false
       t.timestamps
     end
   end
