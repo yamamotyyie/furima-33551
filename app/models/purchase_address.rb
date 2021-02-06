9abc3d52f75144639f500b73036e02af9c66ca93
yamamotyyie <remuxia1415erina@gmail.com>
2021-02-06 20:20:13 +0900

    バリデーション

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,6 +8,8 @@ class PurchaseAddress
     validates :address
     validates :phone_number,format:{with: /\A\d{10}\z|\A\d{11}\z/}
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   validates :prefecture_id,numericality: {other_than:1}
