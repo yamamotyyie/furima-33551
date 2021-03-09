83015f5809e85bc49262a1a60ce641a6bd195949
yamamotyyie <remuxia1415erina@gmail.com>
2021-03-09 23:50:54 +0900

    Update preview.js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change',function(e){
-    
-  })
+    console.log(123);
+  });
 });
